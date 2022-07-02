bd4fd0a47c4cb1d42284398752d2dcd2a2315f8d
vaisakh-evolvfit <vaisakhnp0700@gmail.com>
2022-07-02 22:57:11 +0530

    dark mode

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,21 +1,27 @@
 import React, { useState } from "react";
+import { useTheme } from "next-themes";
 
 export const NavBar = () => {
   const [darkTheme, setDarkTheme] = useState(false);
   function changeState() {
-    setDarkTheme(!darkTheme);
+    if (darkTheme === false) {
+      document.documentElement.classList.add("dark");
+      setDarkTheme(true);
+    } else {
+      document.documentElement.classList.remove("dark");
+      setDarkTheme(false);
+    }
     console.log({ darkTheme });
   }
   return (
-    // <div>
     <div className="flex justify-around py-5 items-center flex-row">
-      <div className="flex flex-col border dark:text-white">
-        <div className="text-xl font-bold">Vaisakh NP</div>
-        <div className="text-lg font-semibold mx-auto">
+      <div className="flex flex-col border dark:text-black text-white">
+        <div className="text-2xl font-bold">Vaisakh NP</div>
+        <div className="text-xl font-semibold mx-auto">
           Full-Stack Developer
         </div>
       </div>
-      <div className="flex flex-row items-center dark:text-white text-lg border">
+      <div className="flex flex-row items-center dark:text-black text-white text-xl border">
         <div className="text-center mx-4">Experience</div>
         <div className="text-center mx-4">Projects</div>
         <div className="text-center mx-4">About</div>
@@ -31,9 +37,13 @@ export const NavBar = () => {
               onChange={changeState}
               defaultChecked={darkTheme}
             />
-            <div className="block bg-gray-300 w-14 h-8 rounded-full"></div>
             <div
-              className={`dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition ${
+              className={`block bg-gray-300 w-14 h-8 rounded-full  ${
+                darkTheme == true && " bg-green-400"
+              }`}
+            ></div>
+            <div
+              className={`absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition ${
                 darkTheme == true && "translate-x-full bg-yellow-400"
               }`}
             ></div>
@@ -41,6 +51,5 @@ export const NavBar = () => {
         </label>
       </div>
     </div>
-    // </div>
   );
 };
