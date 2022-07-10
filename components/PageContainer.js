b256c6cbd2ea62695bba2e5b8985f920efd2f567
cr1spyvada <vaisakhnp0700@gmail.com>
2022-07-10 11:29:52 +0530

    about section mods

--- components/PageContainer.js
+++ components/PageContainer.js
@@ -4,7 +4,7 @@ import { NavBar } from "./NavBar";
 
 export const PageContainer = ({ children }) => {
   return (
-    <div className="bg-sky-200 dark:bg-sky-900 px-20">
+    <div className="bg-sky-200 dark:bg-slate-900 px-20">
       <NavBar />
       <div>{children}</div>
       <Footer />
--- components/Pages/About.js
+++ components/Pages/About.js
@@ -2,43 +2,43 @@ import React from "react";
 
 export const About = () => {
   return (
-    <div className="border my-10 flex justify-around items-centers h-screen dark:text-white">
-      <div className=" my-auto text-center w-1/3 text-6xl">ABOUT</div>
-      <div className="w-2/3 border  mx-4 text-left h-full flex flex-col justify-around font-light text-lg">
-        <div>
-          <div className="font-normal text-xl py-2 text-green-900 dark:text-green-100">
+    <div className="border my-10 flex justify-between items-center h-screen dark:text-white">
+      <div className=" my-auto text-center w-1/3 text-6xl border">ABOUT</div>
+      <div className="w-2/3 border ml-4 text-left h-full flex flex-col justify-around font-light text-lg">
+        <div className="items-end flex flex-col">
+          <div className="font-normal w-full text-left text-xl py-2 text-gray-900 dark:text-sky-200 font-semibolad">
             Me,Myself & I
           </div>
-          <ul>
-            <li className="list-none">
-              Currently in my 4th Year pursuing BTech Computer Science from IIIT
-              Kottayam.
+          <ul className="w-3/4">
+            <li className="list-none my-3">
+              Currently in my 4th Year pursuing BTech Computer Science from IIIT Kottayam.
             </li>
-            <li className="list-none">
+            <li className="list-none my-3">
               Dabble in Web Development, Web Designing, Graphic Designing.
             </li>
-            <li className="list-none">
-              Building effecient and scalable applications, while constantly
-              learning new skills.
+            <li className="list-none my-3">
+              Building effecient and scalable applications, while constantly learning new skills.
             </li>
           </ul>
         </div>
-        <div>
-          <div className="font-normal text-xl  py-6 pb-2 text-green-900 dark:text-green-100">
+        <div className="border">
+          <div className="font-normal w-full text-left text-xl py-2 text-gray-900 dark:text-sky-200 font-semibold">
             Skills
           </div>
           <ul>
             <li className="list-none flex justify-between">
-              <a className="font-normal">Languages:</a> JavaScript, Python, C,
-              C++ , HTML, CSS, SQL, Bash, Java
+              <a className="font-normal w-1/4">Languages:</a>
+              <a className="w-3/4">JavaScript, Python, C, C++ , HTML, CSS, SQL, Bash, Java</a>
             </li>
             <li className="list-none flex justify-between">
-              <a className="font-normal">Frameworks:</a> React, React-Native,
-              NodeJS,ExpressJS, Selenium,Numpy,Pandas
+              <a className="font-normal w-1/4">Frameworks:</a>
+              <a className="w-3/4">React, React-Native, NodeJS,ExpressJS, Selenium,Numpy,Pandas</a>
             </li>
             <li className="list-none flex justify-between">
-              <a className="font-normal">Tools:</a> GIT, Google Cloud, Firebase,
-              MySQL, Expo-CLI, Vercel, Jupyter, MongoDB
+              <a className="font-normal w-1/4">Tools:</a>
+              <a className="w-3/4">
+                GIT, Google Cloud, Firebase, MySQL, Expo-CLI, Vercel, Jupyter, MongoDB
+              </a>
             </li>
           </ul>
         </div>
