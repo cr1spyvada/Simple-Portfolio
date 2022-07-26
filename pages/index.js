9c386671963ba02392558ad86aa7b9df305f3b0e
cr1spyvada <vaisakhnp0700@gmail.com>
2022-07-26 17:16:23 +0530

    case sens issue fix

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Hero } from "../components/Pages/Hero";
+import { Hero } from '../components/sections/Hero';
 import { PageContainer } from "../components/PageContainer";
 import styles from "../styles/Home.module.css";
-import { About } from "../components/pages/About";
-import { Projects } from '../components/Pages/Projects';
+import { About } from '../components/sections/About';
+import { Projects } from '../components/sections/Projects';
 import { NavBar } from '../components/NavBar';
 import { Footer } from '../components/Footer';
-import Experience from '../components/Pages/Experience';
+import Experience from '../components/sections/Experience';
 
 export default function Home() {
   return (
