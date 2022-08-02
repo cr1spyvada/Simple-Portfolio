7c6fddd1f61b7b4bb76816ad3bcfd6bbe2c1e0c0
cr1spyvada <vaisakhnp0700@gmail.com>
2022-08-02 23:50:58 +0530

    viewport fix

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import Head from "next/head";
-import Image from "next/image";
+import Head from 'next/head';
+import Image from 'next/image';
 import { Hero } from '../components/sections/Hero';
-import { PageContainer } from "../components/PageContainer";
-import styles from "../styles/Home.module.css";
+import { PageContainer } from '../components/PageContainer';
+import styles from '../styles/Home.module.css';
 import { About } from '../components/sections/About';
 import { Projects } from '../components/sections/Projects';
 import { NavBar } from '../components/NavBar';
@@ -12,6 +12,9 @@ import Experience from '../components/sections/Experience';
 export default function Home() {
   return (
     <div className="bg-sky-200 sm:text-sm dark:bg-slate-900 px-20">
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
       <NavBar />
       <div>
         <Hero />
