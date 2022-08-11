0a0853aa9a5724370c4c1786d0429b134155ab6d
cr1spyvada <vaisakhnp0700@gmail.com>
2022-08-12 01:31:01 +0530

    preloader first iteration

---
+++ components/SplashScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SplashScreen = () => {
+  return (
+    <div className="bg-black h-screen w-screen flex justify-center items-center">
+      <div className="h-20 w-20 border-4 rounded-lg border-white animate-[spin_2s_linear_infinite]"></div>
+    </div>
+  );
+};
+
+export default SplashScreen;
