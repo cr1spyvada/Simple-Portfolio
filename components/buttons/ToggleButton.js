7ff47941b462951c83832153addbc58cb668a90a
cr1spyvada <vaisakhnp0700@gmail.com>
2022-08-19 23:06:10 +0530

    color scheme update for hero section, page title

--- components/buttons/ToggleButton.js
+++ components/buttons/ToggleButton.js
@@ -6,7 +6,7 @@ const ToggleButton = ({ icon, onClick }) => {
     <button onClick={onClick} className="rounded-full bg-w6 dark:bg-w1 h-8 w-8 md:h-12 md:w-12">
       <FontAwesomeIcon
         icon={icon}
-        className="dark:text-w6 text-w1 text-[1.2rem] md:text-[1.5rem]"
+        className="dark:text-w2 text-w1 text-[1.2rem] md:text-[1.5rem]"
       />
     </button>
   );
--- components/sections/Hero.js
+++ components/sections/Hero.js
@@ -23,7 +23,9 @@ export const Hero = () => {
         <div className="rounded-lg dark:bg-transparent px-5 w-4/5 md:w-full font-light">
           <div className="text-lg font-normal mb-2">Hi, my name is</div>
           <div className="text-xl font-thin text-right flex flex-col items-end">
-            <div className="font-bold md:text-6xl text-4xl mb-4">Vaisakh Nalu Purakkal.</div>
+            <div className="font-bold md:text-6xl dark:text-w2 text-4xl mb-4">
+              Vaisakh Nalu Purakkal.
+            </div>
             <div className="hero-target-container w-fit font-semibold mx-2">
               <div className="text-2xl hero-target">
                 <div className="hero-card-a w-fit">Software Developer</div>
@@ -32,7 +34,7 @@ export const Hero = () => {
                 </div>
               </div>
             </div>
-            <div>designing & building responsive applications every moment.</div>
+            <div>designing & building responsive applications every moment</div>
             <a
               href="mailto:vaisakh2019@iiitkottayam.ac.in"
               className="cursor-pointer font-semibold hover:dark:text-w1 hover:text-w5 w-fit flex items-center flex-nowrap my-2 text-w4 dark:text-w2">
