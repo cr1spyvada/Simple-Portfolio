209d00b52f2e2d39592ba1d86079a6247a4adfba
cr1spyvada <vaisakhnp0700@gmail.com>
2022-08-22 01:46:57 +0530

    color change

--- pages/index.js
+++ pages/index.js
@@ -62,7 +62,7 @@ export default function Home() {
     default: {
       x: mousePosition.x - 16,
       y: mousePosition.y - 16,
-      backgroundColor: darkTheme ? '#C57B57' : '#5C80BC',
+      backgroundColor: darkTheme ? '#C57B57' : '#F0F9F6',
       mixBlendMode: 'difference'
     },
     text: {
