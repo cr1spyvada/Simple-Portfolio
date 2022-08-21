25d66f0cec5a76016492108a0f47634632ce84c0
cr1spyvada <vaisakhnp0700@gmail.com>
2022-08-21 13:29:01 +0530

    final loading screen

--- components/SplashScreen.js
+++ components/SplashScreen.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const SplashScreen = () => {
   return (
-    <div className="bg-black h-screen w-screen flex justify-center items-center">
-      <div className="h-20 w-20 border-4 rounded-lg border-white animate-[spin_2s_linear_infinite]"></div>
+    <div className="bg-w1 h-screen w-screen flex flex-col justify-center items-center">
+      {/* <div className="h-20 w-20 border-8 bg-gradient-to-t from-blue-500 to-green-400 rounded-lg border-white animate-[spin_2s_linear_infinite]"></div> */}
+      <div className="bg-w6 rounded-full h-8 w-8 mb-1 transition animate-bounceY delay-[1000ms]" />
+      <div className="bg-w2 rounded-full h-8 w-8 transition animate-bounce" />
+      <div className="bg-w4 rounded-full h-8 w-8" />
     </div>
   );
 };
