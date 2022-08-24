3928777e866aa3c0ebf93862a5b35beb82fa226a
cr1spyvada <vaisakhnp0700@gmail.com>
2022-08-24 18:15:09 +0530

    card-bg update

--- components/PageContainer.js
+++ components/PageContainer.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export const PageContainer = ({ height, children, title, color = 'white' }) => {
+export const PageContainer = ({ height, children, title, color = 'bg-w1 dark:bg-transparent' }) => {
   return (
     <div
       id={title}
       className={`
-      ${height} md:w-[75vw] md:backdrop-blur-3xl md:shadow-lg rounded-xl mx-auto md:h-screen flex flex-col justify-center px-10 md:px-20 dark:text-w1 ${
+      ${height} md:w-[75vw] md:backdrop-blur-3xl dark:border-w2 border-2 daark:shadow-[#bfbebe]  md:shadow-lg rounded-xl mx-auto md:h-screen flex flex-col justify-center px-10 md:px-20 dark:text-w1 ${
         title ? 'py-20' : 'py-0'
       } ${color}`}>
       {title && (
@@ -13,7 +13,7 @@ export const PageContainer = ({ height, children, title, color = 'white' }) => {
           {title?.toUpperCase()}
         </div>
       )}
-      <div className="mb-auto p-3 md:p-0 backdrop-blur-3xl rounded-xl shadow-lg md:shadow-none md:backdrop-blur-none md:my-auto h-5/6 w-full">
+      <div className="mb-auto p-3 md:p-0 backdrop-blur-3xl shadow-w6 rounded-xl shadow-lg md:shadow-none md:backdrop-blur-none md:my-auto h-5/6 w-full">
         {children}
       </div>
     </div>
--- components/sections/Hero.js
+++ components/sections/Hero.js
@@ -18,23 +18,20 @@ export const Hero = () => {
     });
   }, []);
   return (
-    <PageContainer height="h-screen">
-      <div className="h-full w-full text-right flex justify-center items-start">
+    <PageContainer height="h-[75vh] md:h-[75vh]">
+      <div className="h-full w-full text-right flex justify-center items-center">
         <div className="rounded-lg dark:bg-transparent px-5 w-4/5 md:w-full font-light">
-          <div className="text-lg font-normal mb-2">Hi, my name is</div>
-          <div className="text-xl font-thin text-right flex flex-col items-end">
-            <div className="font-bold md:text-6xl text-w6 dark:text-w2 text-4xl mb-4">
-              Vaisakh Nalu Purakkal.
-            </div>
+          <div className="text-xl font-normal mb-2">Hi, I'm</div>
+          <div className="text-2xl font-thin text-right flex flex-col items-end">
+            <div className="font-bold md:text-9xl text-w6 dark:text-w2 text-5xl mb-4">Vaisakh</div>
             <div className="hero-target-container w-fit font-semibold mx-2">
-              <div className="text-2xl hero-target">
+              <div className="md:text-4xl text-2xl hero-target">
                 <div className="hero-card-a w-fit">Software Developer</div>
                 <div className="hero-card-b w-fit whitespace-pre-wrap xs:whitespace-nowrap">
                   {'Problem\nSolver'}
                 </div>
               </div>
             </div>
-            <div>designing & building responsive applications every moment</div>
             <a
               href="mailto:vaisakh2019@iiitkottayam.ac.in"
               className="cursor-pointer font-semibold hover:dark:text-w1 hover:text-w5 w-fit flex items-center flex-nowrap my-2 text-w4 dark:text-w2">
