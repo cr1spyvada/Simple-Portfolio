c66c4349286938b58659ded7028dea445eee9b1c
cr1spyvada <vaisakhnp0700@gmail.com>
2024-10-20 17:22:26 +0530

    fix: alighnment footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,10 +5,10 @@ import { FaGithub, FaHashnode, FaLinkedin } from 'react-icons/fa6';
 import { RiNextjsFill, RiTailwindCssFill } from 'react-icons/ri';
 
 export const Footer = ({ textEnter, textLeave }) => {
-  const IconClassName = 'w-full fill-black h-8 mx-4 my-2 rounded-lg dark:fill-white';
+  const IconClassName = 'w-full fill-black h-8 my-2 rounded-lg dark:fill-white';
   return (
     <div className="px-20 flex flex-wrap gap-1 justify-between items-center text-sm md:text-xl dark:text-w1 text-w6">
-      <div className="flex justify-around w-full md:w-fit">
+      <div className="flex justify-around w-full md:w-fit gap-8">
         {/* TODO: add to meta doc */}
         <a
           onMouseOver={() => textEnter(2)}
@@ -35,11 +35,11 @@ export const Footer = ({ textEnter, textLeave }) => {
           <FaHashnode className={IconClassName} />
         </a>
       </div>
-      <div className="px-2 flex flex-row items-center justify-center w-full md:w-fit">
+      <div className="px-2 my-2 flex flex-row items-center justify-center w-full md:w-fit">
         Developed with
-        <RiNextjsFill className="fill-w6 w-8 m-2 rounded-lg dark:fill-w1" />
+        <RiNextjsFill className="fill-w6 w-8 rounded-lg dark:fill-w1" />
         ,
-        <RiTailwindCssFill className="fill-w6 w-10 m-2 rounded-lg dark:fill-w1" />& ❤️
+        <RiTailwindCssFill className="fill-w6 w-10 rounded-lg dark:fill-w1" />& ❤️
       </div>
       <div className="w-full pb-4 flex justify-center items-center">
         <a
