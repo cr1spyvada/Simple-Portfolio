459ddb3790bcf4b4a13ed99afe2ee8a6831c3864
cr1spyvada <vaisakhnp0700@gmail.com>
2024-10-20 16:51:50 +0530

    fix: button size

--- components/buttons/ToggleButton.js
+++ components/buttons/ToggleButton.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 const ToggleButton = ({ icon, onClick }) => {
   return (
-    <button onClick={onClick} className="rounded-full bg-w6 dark:bg-w1 h-4 w-4 md:h-8 md:w-8">
-      <FontAwesomeIcon icon={icon} className="dark:text-w2 text-w1 text-[1rem] md:text-[1rem]" />
+    <button onClick={onClick} className="rounded-full bg-w6 dark:bg-w1 h-8 w-8">
+      <FontAwesomeIcon icon={icon} className="dark:text-w2 text-w1 text-[1.25rem] md:text-[1rem]" />
     </button>
   );
 };
